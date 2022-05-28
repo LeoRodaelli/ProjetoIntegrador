d3df11e0077f990c8539920ec2f5df517866f9dc
Leonardo Rodaelli <101615835+LeoRodaelli@users.noreply.github.com>
2022-05-27 21:21:35 -0300

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,7 +1,12 @@
 
 CREATE TABLE pessoas
-(NOME varchar2(30),
+(NOME varchar2(30) CONSTRAINT PK_NOME PRIMARY KEY,
+RA number(9));
+
+CREATE TABLE DADOS
+(RA number(9) foreign key references pessoas,
 IDADE number(3),
 SEXO varchar2(1),
 ALTURA number(3),
 PESO number(3));
+
